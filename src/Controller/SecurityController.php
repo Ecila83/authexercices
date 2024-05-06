258c33b7baf8f0bc1e3bbfc0fc8cfcc6432b9944
Alice Marique <mariquealice83@gmail.com>
2024-05-06 12:10:29 +0200

    agencement inscription +hash mot de passe

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/inscription', name: 'inscription')]
+    public function index(EntityManagerInterface $em, Request $request,UserPasswordHasherInterface $password): Response
+    {
+        $user = new User();
+        $userForm = $this->createForm(UserType::class, $user);
+        $userForm->handleRequest($request);
+
+        if($userForm->isSubmitted() && $userForm->isValid()){
+            $user->setPassword($password->hashPassword($user, $user->getPassword()));
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('security/inscription.html.twig', [
+            'form' => $userForm->createView(),
+        ]);
+    }
+}
