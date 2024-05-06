f131e5d7619dbd1ec556c0a1cdb4c1375d8aa402
Alice Marique <mariquealice83@gmail.com>
2024-05-06 16:02:03 +0200

    connexion securité d'authentification

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,11 +10,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/inscription', name: 'inscription')]
-    public function index(EntityManagerInterface $em, Request $request,UserPasswordHasherInterface $password): Response
+    public function inscription(EntityManagerInterface $em, Request $request,UserPasswordHasherInterface $password): Response
     {
         $user = new User();
         $userForm = $this->createForm(UserType::class, $user);
@@ -29,5 +30,18 @@ class SecurityController extends AbstractController
         return $this->render('security/inscription.html.twig', [
             'form' => $userForm->createView(),
         ]);
+    }
+
+    #[Route('/connexion', name: 'connexion')]
+    public function connexion(AuthenticationUtils $authenticationUtils): Response
+    {  
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $username = $authenticationUtils->getLastUsername();
+
+
+       return $this->render('security/connexion.html.twig', [
+        'error' => $error,
+        'username' => $username
+       ]);
     }
 }
