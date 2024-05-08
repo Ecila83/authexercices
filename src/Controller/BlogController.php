0fdee1448ca7106bbea9fa1773329cd5f8879c8e
Alice Marique <mariquealice83@gmail.com>
2024-05-08 11:57:38 +0200

    voter partie 1

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Blog;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/blog/edit/{id}', name: 'edit_blog')]
+    #[IsGranted("ROLE_USER")]
+    public function edit(Blog $blog): Response
+    {
+        $this->denyAccessUnlessGranted('BLOG_EDIT');
+
+        $user = $this->getUser();
+
+        if($blog->getAuthor() === $user){
+            dump('edition ok');
+        }else{
+            dump('edition pas ok');
+        }
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    #[Route('/test', name: 'test')]
+    public function test(EntityManagerInterface $em, UserRepository $userRepo): Response
+    {
+        $user = $userRepo->find(1);
+        $blog = new Blog();
+        $blog->setContent('123');
+        $blog->setAuthor($user);
+        $em->persist($blog);
+        $em->flush();
+
+        return $this->redirectToRoute('home');
+    }
+}
+
