0893e26d7f06ad983f3caa6b120d6942f6439f38
Alice Marique <mariquealice83@gmail.com>
2024-05-30 16:47:46 +0200

    listener

---
+++ src/EventListener/loginListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+class LoginListener {
+    public function loginFlash(LoginSuccessEvent $event) {
+        $request = $event->getRequest();
+        $userName = $event->getPassport()->getUser()->getUsername(); // Assuming getUser() returns a User object
+        $request->getSession()->getFlashBag()->add('success', 'Bon retour parmi nous  ' . $userName);
+    }
+}
+
